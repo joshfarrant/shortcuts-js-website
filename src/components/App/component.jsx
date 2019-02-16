4b41559d03a5559076ff8ec9fbbf4b9118aa403b
Josh Farrant <josh@farrant.me>
2019-02-16 10:36:34 +0000

    Tweaked some copy

--- src/components/App/component.jsx
+++ src/components/App/component.jsx
@@ -62,10 +62,8 @@ export default class Component extends React.Component {
             </h2>
             <div className={styles.sectionContent}>
               <p>
-                Shortcuts JS lets you build Shortcuts more efficiently by allowing you
-                to leverage all of the features of JavaScript to generate a Shortcut,
-                allowing you to create complex Shortcuts more quickly and more easily
-                than ever before.
+                Shortcuts JS enables you create Shortcuts using JavaScript,
+                allowing you to build complex Shortcuts more quickly and easily than ever before.
               </p>
               <p>
                 <a
